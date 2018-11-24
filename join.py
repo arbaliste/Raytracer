0051938695910e53796c6493d41791431e17999b
Jaren Mendelsohn <jaren.mendelsohn@gmail.com>
2018-11-24 00:54:31 -0800

    BEAR

--- join.py
+++ join.py
@@ -7,22 +7,25 @@ exclude = ["bear.raw"]
 
 def nsplit(list, n):
     l = []
-    for k in range(len(list)/n):
+    for _ in range(len(list)//n):
         l.append(list[:n])
         list = list[n:]
     return l
+
+def process(num, invert=False):
+    sign = "2" if ("-" in num) != invert else "1"
+    num = num.replace("-", "")
+    sp = num.split(".")
+    if len(sp) < 2:
+        sp.append("")
+    leftdecim = sp[0].rjust(2, "0")
+    rightdecim = sp[1].ljust(6, "0")
+    return leftdecim + rightdecim + sign
 
 for file in os.listdir(rawpath):
     if file not in exclude:
         print("; " + os.path.splitext(file)[0])
         with open(os.path.join(rawpath, file)) as f:
-            for x in f.read().split():
-                sign = "2" if "-" in x else "1"
-                x = x.replace("-", "")
-                sp = x.split(".")
-                if len(sp) < 2:
-                    sp.append("")
-                leftdecim = sp[0].rjust(2, "0")
-                rightdecim = sp[1].ljust(6, "0")
-                print(leftdecim + rightdecim + sign, end="")
+            for x, z, y in nsplit(f.read().split(), 3):
+                print(process(x) + process(y) + process(z, True), end="")
             print()
